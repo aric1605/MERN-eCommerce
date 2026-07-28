36f3370a2b503c2e575fbff178068a73ebe9ca06
Aric <aricvasaya10@gmail.com>
2026-07-28 18:59:45 +0530

    Exposed Mongo URI fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -21,7 +21,6 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI);
 
     const connection = await mongoose.connect(process.env.MONGO_URI);
 
